c00f85e8569f9438b816af6f17f23d938e39569a
quarkusio <no-reply@quarkus.io>
2024-05-11 12:11:30 +0000

    Initial commit

---
+++ src/native-test/java/org/azc/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.azc;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
